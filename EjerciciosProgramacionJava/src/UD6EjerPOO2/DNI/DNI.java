e56fb7f341e2e43d2b24e0dcabeca5dc25ae22b3
iris <isuay2@gmail.com>
2022-01-17 13:10:35 +0100

    Actualizado UD6POO2.

--- EjerciciosProgramacionJava/src/UD6EjerPOO2/DNI/DNI.java
+++ EjerciciosProgramacionJava/src/UD6EjerPOO2/DNI/DNI.java
@@ -2,6 +2,40 @@ package UD6EjerPOO2.DNI;
 
 public class DNI {
 
+    //Variable de DNI
+    private int dni;
+
+    //Constructor
+    public DNI(int d) {
+        dni = d;
+        imprimir();
+    }
+
+    //Setter de los números del DNI
+    public void setDni(int d) {
+        dni = d;
+    }
+
+    //Getter de los números del DNI
+    public int getDni() {
+        return dni;
+    }
+
+    //Método para calcular la letra del DNI
+    public char getLetra() {
+        int res = dni % 23;
+        String letra = "TRWAGMYFPDXBNJZSQVHLCKE";
+        return letra.charAt(res);
+    }
     
+    //Método para juntar los números y la letra del DNI
+    public String getNIF() {
+        String d = String.valueOf(dni);
+        return d + getLetra();
+    }
     
+    //Método para imprimir el DNI
+    public void imprimir() {
+        System.out.println("DNI: " + getNIF());
+    }
 }
--- EjerciciosProgramacionJava/src/UD6EjerPOO2/DNI/Main.java
+++ EjerciciosProgramacionJava/src/UD6EjerPOO2/DNI/Main.java
@@ -3,9 +3,7 @@ package UD6EjerPOO2.DNI;
 public class Main {
 
     public static void main(String[] args) {
-
-        
-        
+        DNI a = new DNI(12345678);
+        DNI b = new DNI(27356383);
     }
-
 }
