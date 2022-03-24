6ea4ac6cc5bacfb884544c82f9c83677da5df7bc
Iris <irisau2@gmail.com>
2022-03-24 20:45:05 +0100

    Creado reto UD9.

---
+++ EjerciciosProgramacionJava/src/UD9RetoCesar/CifradoCesarPro.java
@@ -0,0 +1,50 @@
+package UD9RetoCesar;
+
+import java.io.*;
+import java.util.*;
+
+public class CifradoCesarPro {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        try {
+
+            System.out.print("Dime el archivo quieres descifrar: ");
+            String archivo = in.nextLine();
+
+            File carta = new File(archivo);
+
+            Scanner lector = new Scanner(carta);
+
+            System.out.print("Clave: ");
+            int clave = in.nextInt();
+
+            String linea = lector.nextLine();
+
+            for (int i = 0; i < linea.length(); i++) {
+
+                char c = linea.charAt(i);
+
+                if (c >= 'A' & c <= 'Z') {
+
+                    int letra = (int) c + clave;
+
+                    if (letra > 'Z') {
+                        int num = letra - 90;
+                        letra = 64 + num;
+                    }
+
+                    c = (char) letra;
+                }
+                System.out.print(c);
+            }
+            lector.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: El fichero no existe");
+        } catch (Exception e) {
+            System.err.println("Error: " + e);
+        }
+    }
+}
